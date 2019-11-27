95fe0bea83911b7cc273f3c488efd83496cd2caa
Aleksandr Dyomin <sentdyem@sdfinish.onmicrosoft.com>
2019-11-27 18:45:56 +0300

    Добавьте файлы проекта.

---
+++ Book/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Book.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Book.Controllers
+{
+    public class HomeController : Controller
+    {
+        ContactContext db = new ContactContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult AddContact()
+        {
+            return RedirectToAction("Create");
+        }
+        public ActionResult Contacts()
+        {
+            return View(db.Contacts);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Contact contact)
+        {
+            db.Contacts.Add(contact);
+            db.SaveChanges();
+            return RedirectToAction("Contacts");
+        }
+        
+    
+    }
+}
